d1a5039088cdb141f0303074cd607b1105baf792
Travis Durrant <kinglink89@yahoo.com>
2023-02-05 17:48:38 -0700

    placement imagined

---
+++ app.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+#initialize tk(gui) as window and assign a title
+window = tk.Tk()
+window.title("Crude Auto Clicker")
+
+#! - layout plan indicator
+#! info
+ 
+#! start/stop button
+    #* raised = stopped / sunken = running
+
+#! assigned increase speed keyboard button
+#! reassign text field
+    #* reassign submit button
+#! increase speed button
+
+#! current speed
+    #* flat 
+
+#! assigned increase speed keyboard button
+#! reassign text field
+    #* reassign submit button
+#! decrease speed button
+
+
+
+window.mainloop()
